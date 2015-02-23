93e5b1a6ca5fd12ead4a955c22e03efdcf70b683
xu-hong <xuhong713@gmail.com>
2015-02-23 00:12:12 -0500

    add to Neptune

--- Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -48,7 +48,7 @@ public class ConferenceApi {
 
     // TODO 1 Pass the ProfileForm parameter
     // TODO 2 Pass the User parameter
-    public Profile saveProfile() throws UnauthorizedException {
+    public Profile saveProfile(final User user, ProfileForm profileForm) throws UnauthorizedException {
 
         String userId = null;
         String mainEmail = null;
@@ -57,22 +57,34 @@ public class ConferenceApi {
 
         // TODO 2
         // If the user is not logged in, throw an UnauthorizedException
+        if (user == null){
+        	throw new UnauthorizedException("Authorization Required.");
+        }
 
         // TODO 1
         // Set the teeShirtSize to the value sent by the ProfileForm, if sent
         // otherwise leave it as the default value
+        if ( profileForm.getTeeShirtSize() != null) {
+        	teeShirtSize = profileForm.getTeeShirtSize();
+        }
 
         // TODO 1
         // Set the displayName to the value sent by the ProfileForm, if sent
         // otherwise set it to null
+        displayName = profileForm.getDisplayName();
 
         // TODO 2
         // Get the userId and mainEmail
+        userId = user.getUserId();
+        mainEmail = user.getEmail();
 
         // TODO 2
         // If the displayName is null, set it to default value based on the user's email
         // by calling extractDefaultDisplayNameFromEmail(...)
-
+        if (displayName == null) {
+        	displayName = extractDefaultDisplayNameFromEmail(mainEmail);
+        }
+        
         // Create a new Profile entity from the
         // userId, displayName, mainEmail and teeShirtSize
         Profile profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
