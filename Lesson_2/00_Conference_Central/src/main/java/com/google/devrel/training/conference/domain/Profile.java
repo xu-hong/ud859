5cf7bedc4b21394169a38d3f8964045911e40283
xu-hong <xuhong713@gmail.com>
2015-02-25 17:46:30 -0500

    lesson 3

--- Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ Lesson_2/00_Conference_Central/src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -6,13 +6,14 @@ import com.googlecode.objectify.annotation.Id;
 
 
 // TODO indicate that this class is an Entity
+@Entity
 public class Profile {
 	String displayName;
 	String mainEmail;
 	TeeShirtSize teeShirtSize;
 
 	// TODO indicate that the userId is to be used in the Entity's key
-	String userId;
+	@Id String userId;
     
     /**
      * Public constructor for Profile.
